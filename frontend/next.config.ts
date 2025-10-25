76a9a9a3302ffda03cffa91fa027ebfa319c6548
habtamneshmarye <habtamneshmarye@gmail.com>
2025-10-25 09:37:08 +0300

    updated project

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import path from 'path';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: path.resolve(__dirname), // absolute path to your frontend folder
+  },
 };
 
 export default nextConfig;
